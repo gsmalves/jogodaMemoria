8241b9feb7843beea934abf273b29c8ff4074e28
gsmalves <gustavoeu2017@gmail.com>
2020-11-15 00:00:28 -0300

    final

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ let lockBoard = false;
 function flipcarta(){
     if(lockBoard) return;
     if(this===firstCard) return;
+
     this.classList.add('flip')
     if(!hasFlippedCard){
         hasFlippedCard = true
@@ -18,6 +19,7 @@ function flipcarta(){
     secondCard=this;
     hasFlippedCard=false;
     checkForMatch();
+    
 }
 function checkForMatch(){
     if(firstCard.dataset.carta===secondCard.dataset.carta){
@@ -30,19 +32,33 @@ function checkForMatch(){
 function disableCards(){
     firstCard.removeEventListener('click', flipcarta)
     secondCard.removeEventListener('click', flipcarta)
+    resetBoard();
+
 }
 function unflipCards(){
-    lockBoarda = true;
+    lockBoard = true;
 
     setTimeout(()=> {
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
     
-    lockBoard = false;
+    resetBoard();
     },1500);
 }
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard,secondCard]= [null,null];
+}
 
+(function shuffle(){
+    cartas.forEach((carta)=>{
+        let randomPosition = Math.floor(Math.random() * 12);
+        carta.style.order = randomPosition;
+
+    });
+  
+})();
 cartas.forEach((carta)=>{
     carta.addEventListener('click',flipcarta);
-
+    
 });
